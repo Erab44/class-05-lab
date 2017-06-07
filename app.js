63346b6c1f61ebdebececef30d5569847758d067
Jeff McCord <jeff.mccord44@gmail.com>
2017-06-07 10:37:39 -0700

    added problem 3

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(num1, num2) {
-  var Array1 = [num1 + num2, 'The sum of 4 and 7 is 11'];
-  return Array1;
+  var array1 = [num1 + num2, 'The sum of 4 and 7 is 11'];
+  return array1;
 }
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
@@ -38,7 +38,10 @@ Fourth element: "The product of 4 and 7 and 5 is 140."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-
+function sumAndMultiply(number1, number2, number3) {
+  var arraySumAndMulitply = [number1 + number2 + number3, number1 * number2 * number3, '4 and 7 and 5 sum to 16', 'The product of 4 and 7 and 5 is 140'];
+  return arraySumAndMulitply;
+}
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
 
